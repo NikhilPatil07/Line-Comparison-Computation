8b460f1fc5612fda2236cd7d69772fe3a24399b2
NikhilPatil07 <patilnikhil181804@gmail.com>
2024-02-26 10:41:49 +0530

    [ADDED]: Nikhik -  Line Comparison Computation Project Setup

---
+++ src/com/infogalaxy/lineComparison/LineComparisonJava.java
@@ -0,0 +1,10 @@
+package com.infogalaxy.lineComparison;
+
+public class LineComparisonJava {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(" Wlecome to Line Comparison Computation Program ");
+	}
+
+}
